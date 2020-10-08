371a5189aa3d081b898d2b19385f8850d125ce21
Albert De Leon Jr <Kitamoriken1@gmail.com>
2020-10-08 14:51:46 +0800

    initial commit

---
+++ src/test/java/com/pixelHeartProduction/APIServices/ApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelHeartProduction.APIServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
